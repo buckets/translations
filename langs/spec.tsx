dd84f4b846de8b16b62721f77305615536751bdd
Limezy <arnaudetpia@de-ruffray.fr>
2018-04-26 23:05:16 +0800

    Pull changes
    Pull changes

--- langs/spec.tsx
+++ langs/spec.tsx
@@ -1,18 +1,51 @@
 export { IMessages } from './base';
 import { IMessages } from './base';
 
+export type NumberFormat =
+  | ''
+  | 'comma-period'
+  | 'period-comma'
+  | 'space-comma'
+
+export type NumberFormatDef = {
+  [K in NumberFormat]?: INumberFormat
+}
+
 export interface ILangPack {
   name: string;
   dir: 'ltr'|'rtl';
-  numbers: {
-    group: string;
-    group_regex: RegExp;
-    decimal: string;
-    decimal_regex: RegExp;
-  };
+  numbers: NumberFormat;
   messages: IMessages;
   contributors: Array<{
     name: string;
     href?: string;
   }>;
+}
+
+export interface INumberFormat {
+  group: string;
+  group_regex: RegExp;
+  decimal: string;
+  decimal_regex: RegExp;
+}
+
+export const NUMBER_FORMATS:NumberFormatDef = {
+  'comma-period': {
+    group: ',',
+    group_regex: /,/g,
+    decimal: '.',
+    decimal_regex: /\./g,
+  },
+  'period-comma': {
+    group: '.',
+    group_regex: /\./g,
+    decimal: ',',
+    decimal_regex: /,/g,
+  },
+  'space-comma': {
+    group: ' ',
+    group_regex: /[ ]/g,
+    decimal: ',',
+    decimal_regex: /,/g,
+  }
 }
